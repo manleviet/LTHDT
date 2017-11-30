85dbb7a639a209c5579d040a18100c16b3de2a25
Lê Viết Mẫn <lvman@hce.edu.vn>
2017-11-30 16:16:36 +0700

    Cập nhật Hàm vẽ cho Bài tập năm 2017
    Cập nhật Hàm vẽ cho Bài tập năm 2017

---
+++ Bài tập/2017/SchellingModel/SchellingModel/GridUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchellingModel
+{
+    class GridUtility
+    {
+        public static void DrawGrid(int[,] grid)
+        {
+            if (grid.GetLength(0) > 50 || grid.GetLength(1) > 50)
+                return;
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                    if (grid[i, j] == 0)
+                        Console.Write("O ");
+                    else if (grid[i, j] == 1)
+                        Console.Write("X ");
+                    else
+                        Console.Write("  ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Bài tập/2017/SchellingModel/SchellingModel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchellingModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+            int[,] grid = { {  1, 1,  0,  1,  0 },
+                            { -1, 0,  0,  0,  0 },
+                            {  1, 1, -1, -1, -1 },
+                            {  1, 0,  1,  1,  1 },
+                            {  1, 0,  0, -1,  0 } };
+
+            GridUtility.DrawGrid(grid);
+            Console.ReadKey();
+
+            Console.Clear();
+            int[,] grid1 = { { 1, -1, 0, -1,  0 },
+                             { 0,  0, 0,  0,  0 },
+                             { 1,  1, 1, -1,  1 },
+                             { 1, -1, 1,  1,  1 },
+                             { 1,  0, 0,  0, -1 } };
+
+            GridUtility.DrawGrid(grid1);
+            Console.ReadKey();
+
+            Console.Clear();
+            int[,] grid2 = new int[50, 50];
+
+            for (int i = 0; i < 50; i++)
+                for (int j = 0; j < 50; j++)
+                    grid2[i, j] = 1;
+
+            GridUtility.DrawGrid(grid2);
+
+            Console.ReadKey();
+        }
+    }
+}
